94d507ac8b8f8a8dc9a43f9c0fe05652de2ad632
Lintao <lintao.zhang@thoughtworks.com>
2021-04-14 22:31:48 +0800

    init

---
+++ src/main/kotlin/com/example/javatokotlin/JavatokotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.javatokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavatokotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavatokotlinApplication>(*args)
+}
